8922f3d5583c4ae6c2c393b0e4692500dfdf4cfe
AdrianFN11 <73560651+AdrianFN11@users.noreply.github.com>
2021-06-14 13:52:31 -0300

    Cambio de color de "primary" a "azul"
    Realice el cambio de color, estableciendo el color azul similar a la del icono.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,4 +14,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+
+
+
+
+}
