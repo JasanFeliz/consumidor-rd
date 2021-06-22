0bc683a22d3c05eb855d12395c368089328f84de
AdrianFN11 <73560651+AdrianFN11@users.noreply.github.com>
2021-06-21 23:58:49 -0300

    creacion del page recuperar-contrasena

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,6 +38,10 @@ const routes: Routes = [
     path: 'editar-perfil',
     loadChildren: () => import('./editar-perfil/editar-perfil.module').then( m => m.EditarPerfilPageModule)
   },
+  {
+    path: 'recuperar-contrasena',
+    loadChildren: () => import('./recuperar-contrasena/recuperar-contrasena.module').then( m => m.RecuperarContrasenaPageModule)
+  },
 
   
 
