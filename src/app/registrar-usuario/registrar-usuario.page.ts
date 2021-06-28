a3f6c3519998a949e322d9c4d9ac29cc61c4e87d
AdrianFN11 <73560651+AdrianFN11@users.noreply.github.com>
2021-06-28 20:56:50 -0300

    ....

--- src/app/registrar-usuario/registrar-usuario.page.ts
+++ src/app/registrar-usuario/registrar-usuario.page.ts
@@ -10,7 +10,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class RegistrarUsuarioPage implements OnInit {
 
   usuario = this.fb.group({
-    nombre: ['', [Validators.required, Validators.minLength(4)]],
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
     apellidos: ['', [Validators.required, Validators.minLength(3)]],
     email: ['', Validators.compose([Validators.minLength(10), Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'), Validators.required])],
     contrasena: ['', [Validators.required, Validators.minLength(8)]],
