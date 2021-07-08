b34dffa7fb5dd309d002951fd6a83b2637ed064f
AdrianFN11 <73560651+AdrianFN11@users.noreply.github.com>
2021-07-08 16:52:55 -0300

    ..

--- src/app/ajuste/ajuste.page.ts
+++ src/app/ajuste/ajuste.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ajuste.page.scss'],
 })
 export class AjustePage implements OnInit {
+ 
 
   constructor() { }
 
@@ -17,39 +18,17 @@ export class AjustePage implements OnInit {
 
   toggleTheme(event){
   if(event.detail.checked){
-  document.body.setAttribute('color-theme','dark'); 
+  document.body.setAttribute('color-theme','dark' ); 
   }
   else{
   document.body.setAttribute('color-theme','light');
   }
   }
+
+  
+  
 }
 
-/*
-
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-ajustes',
-  templateUrl: './ajustes.page.html',
-  styleUrls: ['./ajustes.page.scss'],
-})
-export class AjustesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 
-  toggleTheme(event){
-    if(event.detail.checked){
-      document.body.setAttribute('color-theme','dark');
-       }
-       else{
-         document.body.setAttribute('color-theme','light');
-       }
-      }
-}
 
-*/
