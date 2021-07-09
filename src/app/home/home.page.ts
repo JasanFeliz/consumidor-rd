de6e9a806c49d829a6f26c42a5a6e1b620666a9e
paponice <paponice@gmail.com>
2021-07-08 21:09:11 -0400

    implementacion y conexion con la base de datos
    Se instalo el plugin de advance-http Cordova
    Se instalo el plugin de ionic-native/http
    Se creo el base de datos tipo archivo Json.
    Se creo la conexión httpclient.get
    Se agrego la carpeta de imágenes de categorías
    Se agregaron los parámetros para las capturas del texto e imágenes para las categorías desde la base de datos.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -6,9 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  categorias: any = [];
+
+  constructor(
+    private http: HttpClient) { }
+
+    ngOnInit() {
+      this.getCategorias().subscribe(res=>{
+        console.log('res',res);
+        this.categorias = res;
+      });
+    }
+    getCategorias() {return this.http
+      .get('assets/data/database.json')
+      .pipe(
+        map((res: any) =>res.categorias)
+      );
+  }
 
 }
-
